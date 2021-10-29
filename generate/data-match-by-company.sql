667ab8483e0cabdcd79e19a43e67ab50d98bb475
Top Poop <93323279+top-poop@users.noreply.github.com>
2021-10-29 15:22:59 +0100

    count spills not spill records for data match numbers

--- generate/data-match-by-company.sql
+++ generate/data-match-by-company.sql
@@ -1,9 +1,9 @@
-select edm.company_name, 'unmatched' as type, count(*) from edm
+select edm.company_name, 'unmatched' as type, sum(spill_count) as count from edm
 left join consents c on edm.consent_id = c.permit_number
 where c.permit_number is null
 group by edm.company_name
 union all
-select edm.company_name, 'matched' as type, count(*) from edm
+select edm.company_name, 'matched' as type, sum(spill_count) as count from edm
 join consents c on edm.consent_id = c.permit_number
 group by edm.company_name
 order by 1, 2;
