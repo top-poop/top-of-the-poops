0fdf0d975a017e3008233388f7e31c0b846483c9
Top Poop <93323279+top-poop@users.noreply.github.com>
2021-10-30 09:57:00 +0100

    - generate reduced complexity constituency boundaries

---
+++ generate/constituencies.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+import argparse
+import contextlib
+import os
+import re
+import sys
+
+import psycopg2
+
+
+@contextlib.contextmanager
+def smart_open(filename=None):
+    if filename and filename != '-':
+        fh = open(filename, 'w')
+    else:
+        fh = sys.stdout
+
+    try:
+        yield fh
+    finally:
+        if fh is not sys.stdout:
+            fh.close()
+
+
+# https://github.com/zhoujin7/casestyle/blob/master/casestyle.py
+def kebabcase(string):
+    s_list = casepreprocess(string)
+    if s_list:
+        return '-'.join(s_list)
+    return ''
+
+
+def casepreprocess(string):
+    if isinstance(string, str):
+        string = string.strip().replace('-', ' ')
+        if string != '_' * len(string):
+            underscore_at_start = ''
+            if string.startswith('_'):
+                j = 1
+                for i, _ in enumerate(string):
+                    if i < len(string):
+                        if string[i + 1] == string[i]:
+                            j += 1
+                        else:
+                            break
+                underscore_at_start = '_' * j
+            underscore_at_end = ''
+            if string.endswith('_'):
+                j = 1
+                for i in range(len(string) - 1, -1, -1):
+                    if i > 0:
+                        if string[i - 1] == string[i]:
+                            j += 1
+                        else:
+                            break
+                underscore_at_end = '_' * j
+        else:
+            return []
+        string = string.replace('_', ' ')
+        string = '%s%s%s' % (underscore_at_start, string, underscore_at_end)
+        # string = f'{underscore_at_start}{string}{underscore_at_end}'
+        string = re.sub('(.)([A-Z][a-z]+)', r'\1 \2', string)
+        string = re.sub('([a-z0-9])([A-Z])', r'\1 \2', string).lower()
+        s_list = string.split()
+        return s_list
+    else:
+        raise TypeError("casepreprocess() argument must be str")
+
+def iter_row(cursor, size=10):
+    while True:
+        rows = cursor.fetchmany(size)
+        if not rows:
+            break
+        for row in rows:
+            yield row
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="generate constituency shape files")
+    parser.add_argument("output", help="output directory")
+
+    args = parser.parse_args()
+
+    output_dir = args.output
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    with psycopg2.connect(host="localhost", database="gis", user="docker", password="docker") as conn:
+
+        tolerance = 0.005
+
+        sql = f"select pcon20nm as name, " \
+              f"st_npoints(wkb_geometry) as points_original, " \
+              f"st_npoints(st_simplifypreservetopology(wkb_geometry, {tolerance})) as points_reduced, " \
+              f"st_asgeojson(st_simplifypreservetopology(wkb_geometry, {tolerance})) as geometry " \
+              f"from pcon_dec_2020_uk_bfc;"
+
+        with conn.cursor() as cursor:
+            cursor.execute(sql)
+
+            columns = [desc[0] for desc in cursor.description]
+
+            for row in iter_row(cursor, 20):
+                (name, points_original, points_reduced, geom) = row
+                print(f"{name} orig={points_original} reduced={points_reduced}")
+
+                with open(os.path.join(output_dir, f"{kebabcase(name)}.json"), "w") as fp:
+                    fp.write(geom)
---
+++ web/data/generated/constituencies/README.md
@@ -0,0 +1,11 @@
+# Constituency Shapes
+
+These files are derived from OS data
+
+Please use the source data rather than copying...
+
+https://opendata.arcgis.com/api/v3/datasets/19841da5f8f6403e9fdcfb35c16e11e9_0/downloads/data?format=shp&spatialRefId=27700
+
+Source: Office for National Statistics licensed under the Open Government Licence v.3.0
+
+Contains OS data © Crown copyright and database right 2021
