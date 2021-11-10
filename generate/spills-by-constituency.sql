9950679899426a99c9e85a710324e71bfe45e31b
Top Poop <93323279+top-poop@users.noreply.github.com>
2021-11-10 19:12:22 +0000

    add twitter information to constituencies

--- generate/spills-by-constituency.sql
+++ generate/spills-by-constituency.sql
@@ -47,11 +47,13 @@ select
     concat(mps.first_name,' ',mps.last_name) as mp_name,
     mps.party as mp_party,
     mps.uri as mp_uri,
+       mps_twitter.screen_name as twitter_handle,
     edm.company_name           as company,
     sum(edm.spill_count)       as total_spills,
     sum(edm.total_spill_hours) as total_hours
 from edm_consent_view edm
          join grid_constituency on edm.effluent_grid_ref = grid_constituency.grid_ref
          left join mps on pcon20nm = mps.constituency
-group by pcon20nm, edm.company_name, mps.first_name, mps.last_name, mps.party, mps.uri
+         left join mps_twitter on mps.constituency = mps_twitter.constituency
+group by pcon20nm, edm.company_name, mps.first_name, mps.last_name, mps.party, mps.uri, mps_twitter.screen_name
 order by total_hours desc
