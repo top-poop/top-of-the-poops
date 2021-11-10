9d4f62f7ffb1712397aa63c60eca4ad2aa27a7a1
Top Poop <93323279+top-poop@users.noreply.github.com>
2021-11-10 04:59:31 +0000

    don't join edm and edm_consent_view as this was causing a few duplicates.
    the view contains all the records anyway

--- generate/spills-by-constituency.sql
+++ generate/spills-by-constituency.sql
@@ -5,9 +5,8 @@ select con.pcon20nm               as constituency,
        edm.company_name           as company,
        sum(edm.spill_count)       as total_spills,
        sum(edm.total_spill_hours) as total_hours
-from edm
-         join edm_consent_view c on edm.consent_id = c.permit_number
-         join grid_references grid on grid.grid_reference = c.effluent_grid_ref
+from edm_consent_view edm
+         join grid_references grid on grid.grid_reference = edm.effluent_grid_ref
          join pcon_dec_2020_uk_bfc con on st_covers(wkb_geometry, st_setsrid(st_point(grid.lon, grid.lat), 4326))
          left join mps on con.pcon20nm = mps.constituency
 group by con.pcon20nm, edm.company_name, mps.first_name, mps.last_name, mps.party, mps.uri
