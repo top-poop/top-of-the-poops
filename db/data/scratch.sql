77d5152ae05399d5463392fadc6149d3d945ec30
Top Poop <93323279+top-poop@users.noreply.github.com>
2021-11-10 05:11:49 +0000

    remove edm/view duplication

--- db/data/scratch.sql
+++ db/data/scratch.sql
@@ -5,11 +5,11 @@
 -- find nearest constituency, for those points that don't exactly lie within
 -- as constituency boundaries don't include waterways, some rivers/seas lie outside
 
+
 with unmatched as (
-    select distinct edm.consent_id, c.effluent_grid_ref, grid.lat, grid.lon
-    from edm
-             join edm_consent_view c on edm.consent_id = c.permit_number
-             join grid_references grid on grid.grid_reference = c.effluent_grid_ref
+    select distinct edm.consent_id, edm.effluent_grid_ref, grid.lat, grid.lon
+    from edm_consent_view edm
+             join grid_references grid on grid.grid_reference = edm.effluent_grid_ref
              left join pcon_dec_2020_uk_bfc con on st_covers(wkb_geometry, st_setsrid(st_point(grid.lon, grid.lat), 4326))
     where con.pcon20nm is null),
      min_distances as (
