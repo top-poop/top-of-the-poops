60cc453c5e87f05396b1c435e7e3c669d6bb9758
Top Poop <93323279+top-poop@users.noreply.github.com>
2021-11-12 05:22:42 +0000

    basic map app

---
+++ js/Map.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const MapApp = () => {
+  return <div>Map App Here</div>
+}
+
+ReactDOM.render(<MapApp/>, document.getElementById('root'));
