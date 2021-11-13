55542e1c9b8d9a553241b3dbcc02163bf291d665
Top Poop <93323279+top-poop@users.noreply.github.com>
2021-11-13 04:56:49 +0000

    fiddling about with map

--- js/Map.jsx
+++ js/Map.jsx
@@ -4,13 +4,27 @@ import {Loading} from "./loading";
 import {Col, Row} from "react-bootstrap";
 import {ForkMeHero, TitleHero} from "./heroes";
 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 const What = ({data}) => {
 
   const constituencies = new Set(data.map( it => it.constituency))
 
   return <Row>
     <Col>
-      <p>Yo {constituencies.size} </p>
+      <MapContainer center={[51.505, -0.09]} zoom={18} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
+          id='mapbox/outdoors-v10'
+          accessToken="pk.eyJ1IjoidGltZTR0ZWEiLCJhIjoiY2t2Y2g0aXFsMHl4NzMxcGd3djcyOG1qNCJ9.YCoFgOmL5dqrJ9ZD7ozJKQ"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </Col>
     <Col>
       <p>Yo {data.length}</p>
