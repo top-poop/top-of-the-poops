5ae9ac1cfd269729ffba8631dd5073add2f56168
Top Poop <93323279+top-poop@users.noreply.github.com>
2021-11-17 23:20:42 +0000

    remove copy-pasta comments

--- js/Map.jsx
+++ js/Map.jsx
@@ -16,16 +16,16 @@ class ErrorBoundary extends React.Component {
         this.state = {hasError: false};
     }
 
-    static getDerivedStateFromError(error) {    // Update state so the next render will show the fallback UI.
+    static getDerivedStateFromError(error) {
         return {hasError: true};
     }
 
-    componentDidCatch(error, errorInfo) {    // You can also log the error to an error reporting service
+    componentDidCatch(error, errorInfo) {
         console.log(error, errorInfo);
     }
 
     render() {
-        if (this.state.hasError) {      // You can render any custom fallback UI
+        if (this.state.hasError) {
             return <h1>Something went wrong.</h1>;
         }
         return this.props.children;
