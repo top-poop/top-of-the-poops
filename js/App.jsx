7349136c14494769ca7f326040913ca412e37372
Top Poop <93323279+top-poop@users.noreply.github.com>
2022-05-02 11:01:59 +0100

    add social media buttons
    no tracking, thanks

--- js/App.jsx
+++ js/App.jsx
@@ -6,7 +6,7 @@ import {BathingSewage, ShellfishSewage, SpillsByConstituency, SpillsByRiver, Spi
 import {Alert, Card, Col, Container, Row, Table} from "react-bootstrap";
 import {ForkMeHero, TitleHero} from "./heroes";
 import {companies} from "./companies";
-import {twitterURI} from "./twitter";
+import {tweetURI, twitterURI} from "./twitter";
 import {formatNumber, toKebabCase} from "./text";
 import {LoadingCircles, Map, MapMove, Mobile} from "./maps";
 import {ChloroGeo, InfoBox, Legend} from "./chloropleth";
@@ -227,6 +227,52 @@ const BeachMap = () => {
     </Map>
 }
 
+const FacebookShare = () => {
+    // No tracking - https://sharingbuttons.io/
+    return <React.Fragment>
+        <a className="resp-sharing-button__link"
+           href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftop-of-the-poops.org" target="_blank"
+           rel="noopener" aria-label="Share on Facebook">
+            <div className="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--large">
+                <div aria-hidden="true" className="resp-sharing-button__icon resp-sharing-button__icon--solid">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z"/>
+                    </svg>
+                </div>
+                Share on Facebook
+            </div>
+        </a>
+    </React.Fragment>
+}
+
+const TwitterShare = () => {
+    // No tracking - https://sharingbuttons.io/
+    const uri = tweetURI({
+        uri: "https://top-of-the-poops.org",
+        text: "Water companies are dumping #sewage into rivers and bathing areas all over the UK - over 470,000 times in 2021 - it needs to be stopped",
+        tags: ["sewage"],
+        via: "sewageuk"
+    })
+
+    return <React.Fragment>
+        <a className="resp-sharing-button__link"
+           href={uri}
+           target="_blank" rel="noopener" aria-label="Share on Twitter">
+            <div className="resp-sharing-button resp-sharing-button--twitter resp-sharing-button--large">
+                <div aria-hidden="true" className="resp-sharing-button__icon resp-sharing-button__icon--solid">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M23.44 4.83c-.8.37-1.5.38-2.22.02.93-.56.98-.96 1.32-2.02-.88.52-1.86.9-2.9 1.1-.82-.88-2-1.43-3.3-1.43-2.5 0-4.55 2.04-4.55 4.54 0 .36.03.7.1 1.04-3.77-.2-7.12-2-9.36-4.75-.4.67-.6 1.45-.6 2.3 0 1.56.8 2.95 2 3.77-.74-.03-1.44-.23-2.05-.57v.06c0 2.2 1.56 4.03 3.64 4.44-.67.2-1.37.2-2.06.08.58 1.8 2.26 3.12 4.25 3.16C5.78 18.1 3.37 18.74 1 18.46c2 1.3 4.4 2.04 6.97 2.04 8.35 0 12.92-6.92 12.92-12.93 0-.2 0-.4-.02-.6.9-.63 1.96-1.22 2.56-2.14z"/>
+                    </svg>
+                </div>
+                Share on Twitter
+            </div>
+        </a>
+    </React.Fragment>
+
+}
+
 class App extends React.Component {
     render() {
         return <div>
@@ -238,6 +284,12 @@ class App extends React.Component {
             </blockquote>
 
             <Container fluid>
+                <Row className="justify-content-md-center">
+                    <Col md="auto">
+                        <TwitterShare/>
+                        <FacebookShare/>
+                    </Col>
+                </Row>
                 <Row>
                     <Col>
                         <p>
@@ -501,9 +553,15 @@ class App extends React.Component {
                     <Col>
                         <p>Please check out our policy on data re-use and attribution at <a
                             href="https://github.com/top-poop/top-of-the-poops">our GitHub page</a></p>
-                        <p>We've been featured in: <a
-                            href="https://sotn.newstatesman.com/2022/04/mapped-sewage-dumps-and-spills-in-england-and-wales/">The
-                            New Statesman</a> "Mapped: Sewage dumps and spills in England and Wales"</p>
+                        <p>We've been featured in:
+
+                            <ul>
+                                <li><a href="https://sotn.newstatesman.com/2022/04/mapped-sewage-dumps-and-spills-in-england-and-wales/">The New Statesman</a> "Mapped: Sewage dumps and spills in England and Wales" </li>
+                                <li><a href="https://www.theguardian.com/environment/2022/may/02/untreated-sewage-discharge-england-coastal-bathing-waters-2021">The Guardian</a> "Raw sewage ‘pumped into English bathing waters 25,000 times in 2021’"</li>
+                                <li><a href="https://www.bbc.co.uk/news/uk-england-61194173">BBC News</a> "Protests over water firms dumping sewage in rivers"</li>
+                                <li><a href="https://www.independent.co.uk/news/uk/home-news/sewage-shellfish-pollution-england-uk-b1963688.html">The Independent</a> "Shellfish areas polluted by sewage tens of thousands of times last year"</li>
+                            </ul>
+                             </p>
                     </Col>
                 </Row>
 
