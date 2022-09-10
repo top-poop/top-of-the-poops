651076ade6f3cb7727a3d55085cfaf99964e1652
Top Poop <93323279+top-poop@users.noreply.github.com>
2022-09-10 16:30:10 +0100

    counties

--- db/data/update_grid_references.sql
+++ db/data/update_grid_references.sql
@@ -13,9 +13,17 @@ alter table grid_references
     add column
         pcon20nm text;
 
+alter table grid_references
+    add column
+        ctyua21nm text;
+
 update grid_references set pcon20nm = con.pcon20nm
 from pcon_dec_2020_uk_bfc con
  where st_covers(wkb_geometry, point);
+
+update grid_references set ctyua21nm = cty.ctyua21nm
+from ctyua_dec_2021_uk_buc cty
+where st_covers(wkb_geometry, point);
 
 /*
 find nearest constituency, for those points that don't exactly lie within
@@ -50,3 +58,31 @@ from t
 where g.grid_reference = t.grid_reference;
 
 commit;
+
+
+/* same again for counties */
+
+begin;
+
+create temporary table u on commit drop as
+select grid_references.grid_reference,
+       counties.distance,
+       counties.ctyua21nm
+from grid_references
+         cross join lateral (
+    select point <-> cty.wkb_geometry as distance, cty.ctyua21nm
+    from ctyua_dec_2021_uk_buc cty
+    order by distance
+    limit 1
+    ) counties
+where grid_references.ctyua21nm is null
+  and distance < 0.5
+;
+
+update grid_references as g set ctyua21nm = u.ctyua21nm
+from u
+where g.grid_reference = u.grid_reference;
+
+commit;
+
+
