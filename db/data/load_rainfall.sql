bf3a77d8759cc7f63b4e204562665183cb19f5be
Top Poop <93323279+top-poop@users.noreply.github.com>
2022-09-10 14:44:03 +0100

    uk rainfall

--- db/data/load_rainfall.sql
+++ db/data/load_rainfall.sql
@@ -1,6 +1,6 @@
 \copy rainfall_stations from 'download/rainfall/stations.csv' delimiter ',' csv header;
 \copy rainfall_readings from 'download/rainfall/rainfall.csv' delimiter ',' csv header;
-
+\copy rainfall_uk from 'download/rainfall/uk.csv' delimiter ',' csv header;
 
 alter table rainfall_stations
     add column
--- db/data/rainfall.sql
+++ db/data/rainfall.sql
@@ -1,5 +1,6 @@
 DROP table if exists rainfall_stations CASCADE;
 DROP table if exists rainfall_readings CASCADE;
+DROP table if exists rainfall_uk CASCADE;
 
 create table rainfall_stations (
     code text primary key,
@@ -16,3 +17,8 @@ create table rainfall_readings (
     constraint rainfall_readings_fk foreign key(code) references rainfall_stations(code)
 );
 
+create table rainfall_uk (
+    year numeric,
+    month numeric,
+    reading numeric
+);
