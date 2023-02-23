0c6d0638a4573120d51a567f5cd332c8fa7a7cf4
Top Poop <93323279+top-poop@users.noreply.github.com>
2023-02-23 00:18:19 +0000

    better bucket

--- api/calendar_bucket.py
+++ api/calendar_bucket.py
@@ -1,6 +1,5 @@
 import datetime
 from collections import defaultdict
-from typing import Optional
 
 import pytest
 
@@ -8,14 +7,18 @@ import pytest
 class DayBucket:
     def __init__(self):
         self.states = defaultdict(lambda: datetime.timedelta(seconds=0))
+        self.total = datetime.timedelta(seconds=0)
 
     def allocate(self, state, delta: datetime.timedelta):
+        self.total += delta
         self.states[state] += delta
         if self.states[state] > datetime.timedelta(days=1):
             raise ValueError("Can only have one day's worth of time in a bucket")
+        if self.total > datetime.timedelta(days=1):
+            raise ValueError("More than 1 day's worth in a single day!")
 
     def totals(self):
-        return self.states
+        return {k: v for (k, v) in self.states.items()}
 
 
 def at_midnight(d: datetime.date):
@@ -23,47 +26,46 @@ def at_midnight(d: datetime.date):
 
 
 class Calendar:
-    def __init__(self, start: datetime.date, end: datetime.date):
-        self.start = start
-        self.end = end
+    def __init__(self, initial_state: str, start: datetime.date):
         self.buckets = defaultdict(lambda: DayBucket())
-        self.last_seen = None
 
-    def add(self, state: str, start: Optional[datetime.datetime], end: datetime.datetime):
-        if start is None:
-            start = at_midnight(self.start)
+        self.current_state = initial_state
+        self.last = at_midnight(start)
 
-        if end is None:
-            end = at_midnight(self.end + datetime.timedelta(days=1))
+    def add(self, state: str, at: datetime.datetime):
 
-        if self.last_seen is not None:
-            if start < self.last_seen:
-                raise ValueError("dates have to be non-overlapping")
+        if at < self.last:
+            raise ValueError(f"Events must be in sequence, last {self.last}, this {at}")
 
-        self.last_seen = end
+        day = self.last.date()
 
-        for i in range((end - start).days + 1):
-            day = start.date() + datetime.timedelta(days=i)
+        while day <= at.date():
 
             bucket = self.buckets[day]
 
             day_midnight = at_midnight(day)
             next_day = day_midnight + datetime.timedelta(days=1)
 
-            if start > day_midnight and end < next_day:
-                delta = end - start
-            elif start > day_midnight:
-                delta = next_day - start
-            elif end < next_day:
-                delta = end - day_midnight
+            if self.last > day_midnight and at < next_day:
+                delta = at - self.last
+            elif self.last > day_midnight:
+                delta = next_day - self.last
+            elif at < next_day:
+                delta = at - day_midnight
             else:
                 delta = next_day - day_midnight
 
-            bucket.allocate(state, delta)
+            if delta.total_seconds() > 0:
+                bucket.allocate(self.current_state, delta)
+
+            day += datetime.timedelta(days=1)
+
+        self.current_state = state
+        self.last = at
 
     def allocations(self):
         dates = sorted(self.buckets.keys())
-        return [(d, self.buckets[d].states) for d in dates]
+        return [(d, self.buckets[d].totals()) for d in dates if self.buckets[d].total.total_seconds() > 0]
 
 
 def date_of(s: str) -> datetime.date:
@@ -84,56 +86,68 @@ def test_day_bucket():
 
 
 def test_calendar_with_single_event_spanning_multiple_days():
-    c = Calendar(date_of("2023-01-01"), date_of("2023-02-22"))
+    c = Calendar("unknown", date_of("2023-01-01"))
 
-    c.add("online", None, datetime_of("2023-01-02 02:00"))
-
+    c.add("online", datetime_of("2023-01-02 02:00"))
+    c.add("now", at_midnight(datetime_of("2023-01-04")))
     b = c.allocations()
-    assert len(b) == 2
-    assert b[0] == (date_of("2023-01-01"), {"online": datetime.timedelta(days=1)})
-    assert b[1] == (date_of("2023-01-02"), {"online": datetime.timedelta(hours=2)})
+
+    assert len(b) == 3
+    assert b[0] == (date_of("2023-01-01"), {"unknown": datetime.timedelta(days=1)})
+    assert b[1] == (
+    date_of("2023-01-02"), {"unknown": datetime.timedelta(hours=2), "online": datetime.timedelta(hours=22)})
+    assert b[2] == (date_of("2023-01-03"), {"online": datetime.timedelta(hours=24)})
 
 
 def test_calendar_with_multiple_events_single_day_same_type():
-    c = Calendar(date_of("2023-01-01"), date_of("2023-02-22"))
-    c.add("online", datetime_of("2023-01-01 00:00"), datetime_of("2023-01-01 02:00"))
-    c.add("online", datetime_of("2023-01-01 22:00"), datetime_of("2023-01-02 00:00"))
+    c = Calendar("unknown", date_of("2023-01-01"))
+    c.add("online", datetime_of("2023-01-01 02:00"))
+    c.add("online", datetime_of("2023-01-02 00:00"))
 
     b = c.allocations()
     assert len(b) == 1
-    assert b[0] == (date_of("2023-01-01"), {"online": datetime.timedelta(hours=4)})
+    assert b[0] == (
+    date_of("2023-01-01"), {"unknown": datetime.timedelta(hours=2), "online": datetime.timedelta(hours=22)})
 
 
 def test_calendar_with_multiple_events_single_day_multiple_types():
-    c = Calendar(date_of("2023-01-01"), date_of("2023-02-22"))
-    c.add("online", datetime_of("2023-01-01 00:00"), datetime_of("2023-01-01 02:00"))
-    c.add("offline", datetime_of("2023-01-01 02:00"), datetime_of("2023-01-01 22:00"))
-    c.add("online", datetime_of("2023-01-01 22:00"), datetime_of("2023-01-02 00:00"))
+    c = Calendar("unknown", date_of("2023-01-01"))
+    c.add("online", datetime_of("2023-01-01 00:00"))
+    c.add("offline", datetime_of("2023-01-01 02:00"))
+    c.add("online", datetime_of("2023-01-01 22:00"))
+    c.add("now", datetime_of("2023-01-02 00:00"))
 
     b = c.allocations()
     assert len(b) == 1
-    assert b[0] == (date_of("2023-01-01"), {"online": datetime.timedelta(hours=4), "offline": datetime.timedelta(hours=20)})
-
-
-def test_calendar_fills_to_end():
-    c = Calendar(date_of("2023-01-01"), date_of("2023-02-22"))
-    c.add("online", datetime_of("2023-01-01 00:00"), datetime_of("2023-01-01 02:00"))
+    assert b[0] == (
+    date_of("2023-01-01"), {"online": datetime.timedelta(hours=4), "offline": datetime.timedelta(hours=20)})
 
 
 def test_rejects_previously_seen():
-    c = Calendar(date_of("2023-01-01"), date_of("2023-02-22"))
-    c.add("online", datetime_of("2023-01-01 00:00"), datetime_of("2023-01-01 02:00"))
-    with pytest.raises(Exception):
-        c.add("whatever", datetime_of("2023-01-01 01:59"), datetime_of("2023-01-01 02:00") )
+    c = Calendar("unknown", date_of("2023-01-01"))
+    c.add("online", datetime_of("2023-01-01 02:00"))
+    with pytest.raises(ValueError):
+        c.add("whatever", datetime_of("2023-01-01 01:59"))
+
+
+def list_item_on(l, d: datetime.date):
+    return list(filter(lambda it: it[0] == d, l))[0]
 
 
 def test_realistic_example():
-    c = Calendar(date_of("2023-01-01"), date_of("2023-02-22"))
+    c = Calendar("unknown", date_of("2022-12-01"))
 
-    c.add("unknown",     None,                               datetime_of("2022-12-23 09:15:00"))
-    c.add("overflowing", datetime_of("2022-12-23 09:15:00"), datetime_of("2022-12-23 10:45:00"))
-    c.add("online",      datetime_of("2022-12-23 10:45:00"), datetime_of("2023-01-08 13:30:00"))
-    c.add("overflowing", datetime_of("2023-01-08 13:30:00"), datetime_of("2023-01-08 14:00:00"))
-    c.add("online",      datetime_of("2023-01-08 14:00:00"), datetime_of("2023-01-19 12:30:00"))
-    c.add("offline",     datetime_of("2023-01-19 12:30:00"), datetime_of("2023-01-23 08:45:00"))
-    c.add("offline",     datetime_of("2023-01-23 08:45:00"), None)
+    c.add("overflowing", datetime_of("2022-12-23 09:15:00"))
+    c.add("online", datetime_of("2022-12-23 10:45:00"))
+    c.add("overflowing", datetime_of("2023-01-08 13:30:00"))
+    c.add("online", datetime_of("2023-01-08 14:00:00"))
+    c.add("offline", datetime_of("2023-01-19 12:30:00"))
+    c.add("offline", datetime_of("2023-01-23 08:45:00"))
+    c.add("now", at_midnight(date_of("2023-02-01")))
+
+    b = c.allocations()
+
+    assert list_item_on(b, date_of("2023-01-19")) == (date_of("2023-01-19"),
+                                                      {"online": datetime.timedelta(hours=12, minutes=30),
+                                                       "offline": datetime.timedelta(hours=11, minutes=30)})
+    assert list_item_on(b, date_of("2023-01-23")) == (date_of("2023-01-23"), {"offline": datetime.timedelta(hours=24)})
