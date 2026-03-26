cddf9bd025e2e5cdae849baaeda06cae601b51da
Top Poop <93323279+top-poop@users.noreply.github.com>
2026-03-26 18:35:01 +0000

    load into db

--- db/data/load_edm_2025.sql
+++ db/data/load_edm_2025.sql
@@ -2,6 +2,6 @@
 delete from edm where reporting_year = 2025;
 
 \copy edm from 'download/edm-2025/edm-consolidated.csv' delimiter ',' csv header;
--- \copy edm from 'download/edm-wales-2024/edm-consolidated.csv' delimiter ',' csv header;
+\copy edm from 'download/edm-wales-2025/edm-consolidated.csv' delimiter ',' csv header;
 
 delete from edm where company_name is null;
